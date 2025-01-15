479e0c0d2ce71db1514d3d7ad8e8cdb6a5fda985
ZenosSeemsOk <150531078+ZenosSeemsOk@users.noreply.github.com>
2025-01-15 20:05:54 +0545

    Changed sprites, Added particle effect  and Cleaned Prefabs

--- Assets/Scripts/Sub-mode 2/Aircraft.cs
+++ Assets/Scripts/Sub-mode 2/Aircraft.cs
@@ -4,12 +4,20 @@ using UnityEngine.UI;
 public class Aircraft : MonoBehaviour
 {
     public Image m_Aircraft;
+    [SerializeField] private ParticleSystem hit_Particle_sytem;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Projectile")
+        if (other.CompareTag("Projectile"))
         {
             m_Aircraft.enabled = true;
+
+            // Instantiate a copy of the particle system to avoid modifying the prefab
+            ParticleSystem particleInstance = Instantiate(hit_Particle_sytem, transform.position, Quaternion.identity);
+            particleInstance.Play();
+
+            // Destroy the instance after 1 second
+            Destroy(particleInstance.gameObject, 1f);
         }
     }
 }
